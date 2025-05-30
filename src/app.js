94fa50a792a95249f0ccf8ce9f8044e7bc1e2ffa
Christian <lvchristian20@gmail.com>
2025-05-30 10:36:34 -0500

    fix: se borro la configuración del CORS en app.js y se creo un .htaccess

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const path = require('path');
 const subscriberRoutes = require('./routes/subscriberRoutes');
 const subscriberAuthRoutes = require('./routes/subscriberAuthRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -17,14 +16,6 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-
-app.use('/public/magazines', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // O tu dominio específico
-    next();
-});
-
-// Sirve la carpeta pública general (opcional si quieres exponer más cosas)
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Rutas
 app.use('/api/subscribers', subscriberRoutes);
